57a40704a6e3d67acbc27f29230094bf9ab14060
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2013-05-20 16:22:28 +0200

    theme first created

---
+++ lib/learnery/theme/version.rb
@@ -0,0 +1,9 @@
+module Learnery
+  module Theme
+    VERSION = "0.0.1"
+
+    SITE_LOCALE      = "de-AT"
+    SITE_NAME        = "Fake Coder Dojo Salzburg"
+    SITE_DESCRIPTION = "ein Entwurf wie die CoderDojo Seite auf Learnery aussehen würde"
+  end
+end
